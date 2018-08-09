dc84f93615cadf48a61a0c455dc25dc79ca3d8c1
A <yuva@lm-maa-22004994.paypalcorp.com>
2018-08-09 13:16:48 +0530

    Adding SplOdometer Class

---
+++ SpecialOdometer.java
@@ -0,0 +1,32 @@
+
+	private int [] odometerReadings;
+
+	public int getIndex()
+	{
+		int index=0;
+		return index;
+	}
+	public int next()
+	{
+		int next=0;
+		return next;
+	}
+	public int previous()
+	{
+		int prev=0;
+		return prev;
+	}
+	public int difference()
+	{
+		int speed1=0,speed2=0;
+		return speed2-speed1;
+	}
+	public static void main(String [] args)
+	{
+		int n=2345;
+		SpecialOdometer1 splOdometer=new SpecialOdometer1();
+		int noOfDigits=splOdometer.getNoOfDigits(n);
+	}
+	public int getNoOfDigits(int n) {
+		return 0;
+	}
